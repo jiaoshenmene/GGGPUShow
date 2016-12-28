f2c867e81eb87aa9df68b23a8c126ecc4276bcfb
dujia <815319775@qq.com>
2016-12-28 16:08:35 +0800

    commit first

---
+++ GGGPUShowTests/GGGPUShowTests.m
@@ -0,0 +1,39 @@
+//
+//  GGGPUShowTests.m
+//  GGGPUShowTests
+//
+//  Created by dujia on 28/12/2016.
+//  Copyright © 2016 dujia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GGGPUShowTests : XCTestCase
+
+@end
+
+@implementation GGGPUShowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
